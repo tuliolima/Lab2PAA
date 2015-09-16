dd8e247f4fa15f70a3a58d36383991440e90c39a
tuliolima <tuliook@hotmail.com>
2015-09-16 15:48:10 -0300

    Criado no lab

---
+++ mestre.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+int main () {
+
+
+  double f (int n, double c) {
+    return pow((double) n, c);
+  }
+  
+  void func_t (int n) {
+    
+    if (n==1) return;
+    
+    for (int cont=1; cont<=a; cont++) {
+      func_t(n/b); 
+    }
+    
+    tempo_inicial = pega_tempo();
+    
+    for ( cont=1; cont<= f(n); ) {
+      cont++
+    }
+    
+    tempo_final = pega_tempo();
+    
+    Calcula_nivel_atual_arvore();
+    
+    registra_trabalho_nivel_atual_arvore(...);
+  }
+
+}
