c4e9d53cdaef42710d325ee63296dd21f4a9df76
tuliolima <tuliook@hotmail.com>
2015-09-23 15:44:12 -0300

    update 23/09/2015

--- mestre.c
+++ mestre.c
@@ -1,32 +1,46 @@
 #include <stdio.h>
+#include <sys/timeb.h>
+#include <math.h>
+    
+double f (double n, double c) {
+    return pow(n, c);
+}
+
+void func_t (int a, double b, double c, double n) {
+
+    int cont;
+    double f_n;
+    struct timeb start, end;
+    int diff;
+
+    if (n<=1) {
+        return;
+    }
+
+    for (cont = 0; cont<a; cont++) {
+        func_t(a, b, c, n/b); 
+    }
+    printf("passou\n");
+    ftime(&start);
+    
+    f_n = f(n, c);
+    printf("%f\n", f_n);
+    for (cont = 0; cont <= f_n;) {
+        cont++;
+    }
+    
+    ftime(&end);
+    diff = (int) (1000.0 * (end.time - start.time) + (end.millitm - start.millitm));
+    
+    /*Calcula_nivel_atual_arvore();*/
+    
+    printf("%u\n", diff);
+}
 
 
 int main () {
-
-
-  double f (int n, double c) {
-    return pow((double) n, c);
-  }
   
-  void func_t (int n) {
-    
-    if (n==1) return;
-    
-    for (int cont=1; cont<=a; cont++) {
-      func_t(n/b); 
-    }
-    
-    tempo_inicial = pega_tempo();
-    
-    for ( cont=1; cont<= f(n); ) {
-      cont++
-    }
-    
-    tempo_final = pega_tempo();
-    
-    Calcula_nivel_atual_arvore();
-    
-    registra_trabalho_nivel_atual_arvore(...);
-  }
+    func_t(4, 2, 3, 5000);
 
+    return 0;
 }
